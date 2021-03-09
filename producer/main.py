33f2c1ae49077e5cdd09bedb2c54e0d802e40146
Stefano Varesi <stefano.varesi@gmail.com>
2021-03-09 12:14:41 +0100

    feat(producer): simple message production, supporting cancellation

--- producer/main.py
+++ producer/main.py
@@ -1,5 +1,10 @@
 import logging
 import sys
+from random import random
+from signal import signal, SIGINT
+
+import zmq
+from zmq import PUSH, Socket, ZMQError
 
 log_handler = logging.StreamHandler(sys.stdout)
 log_handler.setFormatter(logging.Formatter('%(asctime)s - %(processName)s - %(levelname)s - %(message)s'))
@@ -9,10 +14,38 @@ log.addHandler(log_handler)
 log.setLevel(logging.DEBUG)
 
 
-def produce():
-    log.info("starting")
-    log.info("terminating")
+class Producer:
+    producing: bool = True
+    socket: Socket = None
+
+    def run(self):
+        log.info("starting")
+
+        context = zmq.Context()
+
+        with context.socket(PUSH) as socket:
+            self.socket = socket
+            socket.bind("tcp://*:5555")
+
+            while self.producing:
+                message = f"hello world ({random()}"
+                log.debug("producing message [%s]" % message)
+                try:
+                    socket.send(bytes(message, 'UTF-8'))
+                except ZMQError as e:
+                    if e.errno == zmq.ENOTSOCK:
+                        log.debug("socket has been closed, terminating")
+                    else:
+                        log.error(e)
+
+        log.info("terminating")
+
+    def stop_producing(self):
+        self.producing = False
+        self.socket.close()
 
 
 if __name__ == "__main__":
-    produce()
+    producer = Producer()
+    signal(SIGINT, lambda signum, frame: producer.stop_producing())
+    producer.run()
